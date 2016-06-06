24c7fa185814f744d952dad5b1d0276681c0e2dd
Enzo Liu <liuenze6516@gmail.com>
2016-06-06 19:03:51 +0800

    finish monad instance definition

--- src/Course/Monad.hs
+++ src/Course/Monad.hs
@@ -6,7 +6,7 @@
 module Course.Monad(
   Monad(..)
 , join
-, (>>=)  
+, (>>=)
 , (<=<)
 ) where
 
@@ -68,8 +68,9 @@ infixr 1 =<<
   f (a -> b)
   -> f a
   -> f b
-(<*>) =
-  error "todo: Course.Monad#(<*>)"
+  -- fmap bind
+  -- f a -> a -> f b -> f b
+ff <*> a = (\f-> f <$> a) =<< ff
 
 infixl 4 <*>
 
@@ -82,8 +83,7 @@ instance Monad Id where
     (a -> Id b)
     -> Id a
     -> Id b
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance Id"
+  f =<< (Id a) = f a
 
 -- | Binds a function on a List.
 --
@@ -94,8 +94,9 @@ instance Monad List where
     (a -> List b)
     -> List a
     -> List b
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance List"
+  _ =<< Nil = Nil
+  f =<< (x:.xs) = f x ++ (f =<< xs)
+  -- (=<<) = flatMap
 
 -- | Binds a function on an Optional.
 --
@@ -106,8 +107,7 @@ instance Monad Optional where
     (a -> Optional b)
     -> Optional a
     -> Optional b
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance Optional"
+  (=<<) = bindOptional
 
 -- | Binds a function on the reader ((->) t).
 --
@@ -118,8 +118,7 @@ instance Monad ((->) t) where
     (a -> ((->) t b))
     -> ((->) t a)
     -> ((->) t b)
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance ((->) t)"
+  f =<< g = \t-> f (g t) t
 
 -- | Flattens a combined structure to a single structure.
 --
